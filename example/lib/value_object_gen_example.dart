e4dba52a95289b04472572f827cf0e907b9a5096
ryota.suzuki <ryota.suzuki@10x.co.jp>
2023-03-06 10:09:40 +0900

    WIP

--- example/lib/value_object_gen_example.dart
+++ example/lib/value_object_gen_example.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:json_annotation/json_annotation.dart';
 import 'package:value_object_gen/builder.dart';
 part 'value_object_gen_example.g.dart';
@@ -12,19 +14,27 @@ abstract class UserName {
   factory UserName.fromJson(String str) => UserName(str);
 }
 
-@valueObject
-abstract class UserAge {
-  final int _value;
-  UserAge._(this._value);
-
-  factory UserAge(int value) => _UserAgeImpl(value);
-  int toJson() => _value;
-  factory UserAge.fromJson(int value) => UserAge(value);
+@ValueObject2(className: 'UserAge')
+abstract class _UserAgeDef {
+  int get _value;
+  UserAge increment() {
+    return UserAge(_value + 1);
+  }
 }
 
 @JsonSerializable(explicitToJson: true)
 class User {
   final UserName name;
+  final UserAge age;
 
-  User(this.name);
+  User(this.name, this.age);
+}
+
+void main() {
+  final user = User(UserName('string'), UserAge(100));
+  print(_$UserToJson(user));
+  final jsonString = jsonEncode(_$UserToJson(user));
+  final u = (_$UserFromJson(jsonDecode(jsonString)));
+  final age = u.age.increment();
+  print(age);
 }
--- example/lib/value_object_gen_example.g.dart
+++ example/lib/value_object_gen_example.g.dart
@@ -8,10 +8,12 @@ part of 'value_object_gen_example.dart';
 
 User _$UserFromJson(Map<String, dynamic> json) => User(
       UserName.fromJson(json['name'] as String),
+      json['age'],
     );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'name': instance.name.toJson(),
+      'age': instance.age,
     };
 
 // **************************************************************************
@@ -36,20 +38,28 @@ class _UserNameImpl extends UserName {
   }
 }
 
-class _UserAgeImpl extends UserAge {
+// **************************************************************************
+// ValueObject2Generator
+// **************************************************************************
+
+class UserAge extends _UserAgeDef {
+  final int _value;
+
   @override
   bool operator ==(Object o) {
     if (identical(this, o)) return true;
-    return o is _UserAgeImpl && o._value == _value;
+    return o is UserAge && o._value == _value;
   }
 
-  _UserAgeImpl(int value) : super._(value);
+  UserAge(int this._value);
 
   @override
   int get hashCode => _value.hashCode;
 
   @override
-  String toString() {
-    return _value.toString();
-  }
+  String toString() => _value.toString();
+
+  int toJson() => _value;
+
+  factory UserAge.fromJson(int json) => UserAge(json);
 }
